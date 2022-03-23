76d8c4d55e2c2792b63e590ef6b5e269d1ea8c37
EmilyGMG <emily.maria2010@hotmail.com>
2022-03-23 08:27:00 -0300

    done

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,22 @@
-import Document from 'next/document';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
-  // render() {
-  //   //TODO
-  // }
+  render() {
+    return(
+      <Html lang="pt-BR">
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&family=Montserrat:wght@400;600;700&family=Poppins:wght@400;600&display=swap" rel="stylesheet" /> 
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+        <script 
+          async 
+          defer 
+          src="https://static.cdn.prismic.io/prismic.js?new=true&repo=testeblog01" />
+      </body>
+      </Html>
+    )
+  }
 }
